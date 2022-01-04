4e7524f566e4df32f464bcafb3a4ff3d66eb2bcc
Sanhamm <sanhamm097@gmail.com>
2022-01-04 12:16:11 +0100

    aSAA

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("hello wordl from my new js file");
+//dette er et regnestykke
+console.log(10009 + 5682);
+/* dette er e stor setning for å prøve å lage mye flere linjer 
+dweiubdfiuewfiubwbfiwebfiweubfewfwefwe
+*/
